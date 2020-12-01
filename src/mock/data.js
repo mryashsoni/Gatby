9623daa88e40711f591470257f81e8a9446e43b3
yash-soni-vst-au4 <42246474+yash-soni-vst-au4@users.noreply.github.com>
2020-12-01 12:03:45 +0530

    message

--- src/mock/data.js
+++ src/mock/data.js
@@ -10,56 +10,56 @@ export const headData = {
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Yash Soni',
+  subtitle: 'I am Web Developer',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'yash.jpg',
+  paragraphOne: 'Yash is a self-development enthusiast and passionate about life-long learning. I believe in the power of technology to transform and improve the lives of people around the world.',
+  paragraphTwo: 'My Professional Skills: A Web Developer ready to build professional websites from the bottom up.',
+  paragraphThree: 'Able to complete projects efficiently with attractive, user-friendly websites. Fast learner, result oriented, and team player who is proficient in an array of scripting languages.',
+  resume: 'https://www.google.com/', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'project1.jpg',
+    title: 'Fully Functional and Responsive Ecommerce Application',
+    info: '.',
+    info2: ' .',
+    url: 'https://commerce-js.netlify.app',
+    repo: 'https://github.com/mryashsoni/ecommerce', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'project2.png',
+    title: 'Mini Projects',
+    info: '.',
+    info2: '.',
+    url: 'https://html-css-mini-projects.netlify.app',
+    repo: 'https://github.com/mryashsoni/mini-projects', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'project3.png',
+    title: 'Covid Meter',
+    info: '.',
+    info2: '.',
+    url: 'https://covid19statswebsite.netlify.app/',
+    repo: 'https://github.com/mryashsoni/covidmeter', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: '+91-8770569251',
+  btn: 'yashsoni1516@gmail.com',
+  email: 'yashsoni@gmail.com',
 };
 
 // FOOTER DATA
@@ -83,7 +83,7 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/mryashsoni',
     },
   ],
 };
